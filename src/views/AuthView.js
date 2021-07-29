aaa744b49d10e59b4cb0f203e42643fd83bca647
PdoubleU <piotrwitasik1988@gmail.com>
2021-07-29 19:47:59 +0200

    [providers]: set up game context and provider 	- method startTimer 	- state timer

--- src/views/AuthView.js
+++ src/views/AuthView.js
@@ -4,6 +4,7 @@ import DisplayForm from '../components/form/DisplayForm';
 import DisplayDictionary from '../components/dictionary/DisplayDictionary';
 import DisplayGame from '../components/game/DisplayGame';
 import { DictionaryProvider } from '../providers/DictionaryProvider';
+import { GameContext, GameProvider } from '../providers/GameProvider';
 import { Route, Link, useLocation } from 'react-router-dom';
 
 const AuthView = () => {
@@ -13,24 +14,26 @@ const AuthView = () => {
   console.log('rerender auth viewe');
   return (
     <DictionaryProvider>
-      <Route path={rootPath}>
-        {pathname === rootPath ? (
-          <nav>
-            <Link to="/add">
-              <StyledButton>Add word</StyledButton>
-            </Link>
-            <Link to="/game">
-              <StyledButton>Play game</StyledButton>
-            </Link>
-            <Link to="/dictionary">
-              <StyledButton>Dictionary</StyledButton>
-            </Link>
-          </nav>
-        ) : null}
-        <Route path="/add" component={DisplayForm} />
-        <Route path="/dictionary" component={DisplayDictionary} />
-        <Route path="/game" component={DisplayGame} />
-      </Route>
+      <GameProvider>
+        <Route path={rootPath}>
+          {pathname === rootPath ? (
+            <nav>
+              <Link to="/add">
+                <StyledButton>Add word</StyledButton>
+              </Link>
+              <Link to="/game">
+                <StyledButton>Play game</StyledButton>
+              </Link>
+              <Link to="/dictionary">
+                <StyledButton>Dictionary</StyledButton>
+              </Link>
+            </nav>
+          ) : null}
+          <Route path="/add" component={DisplayForm} />
+          <Route path="/game" component={DisplayGame} />
+          <Route path="/dictionary" component={DisplayDictionary} />
+        </Route>
+      </GameProvider>
     </DictionaryProvider>
   );
 };
