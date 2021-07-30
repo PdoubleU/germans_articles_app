424ab939cbfb2b470ae6e5df55ac11d03f527d98
PdoubleU <piotrwitasik1988@gmail.com>
2021-07-30 21:15:45 +0200

    [providers]: remove GameProvider - game logic is coded in the DisplaGame component [components]: 	- display game gets methods to handle main features: 		- display coundown (seconds left to choose answer) 		- display Play button 		- display current word to match with the article 		- display buttons DER, DIE, DAS

--- src/views/AuthView.js
+++ src/views/AuthView.js
@@ -4,7 +4,6 @@ import DisplayForm from '../components/form/DisplayForm';
 import DisplayDictionary from '../components/dictionary/DisplayDictionary';
 import DisplayGame from '../components/game/DisplayGame';
 import { DictionaryProvider } from '../providers/DictionaryProvider';
-import { GameContext, GameProvider } from '../providers/GameProvider';
 import { Route, Link, useLocation } from 'react-router-dom';
 
 const AuthView = () => {
@@ -14,26 +13,24 @@ const AuthView = () => {
   console.log('rerender auth viewe');
   return (
     <DictionaryProvider>
-      <GameProvider>
-        <Route path={rootPath}>
-          {pathname === rootPath ? (
-            <nav>
-              <Link to="/add">
-                <StyledButton>Add word</StyledButton>
-              </Link>
-              <Link to="/game">
-                <StyledButton>Play game</StyledButton>
-              </Link>
-              <Link to="/dictionary">
-                <StyledButton>Dictionary</StyledButton>
-              </Link>
-            </nav>
-          ) : null}
-          <Route path="/add" component={DisplayForm} />
-          <Route path="/game" component={DisplayGame} />
-          <Route path="/dictionary" component={DisplayDictionary} />
-        </Route>
-      </GameProvider>
+      <Route path={rootPath}>
+        {pathname === rootPath ? (
+          <nav>
+            <Link to="/add">
+              <StyledButton>Add word</StyledButton>
+            </Link>
+            <Link to="/game">
+              <StyledButton>Play game</StyledButton>
+            </Link>
+            <Link to="/dictionary">
+              <StyledButton>Dictionary</StyledButton>
+            </Link>
+          </nav>
+        ) : null}
+        <Route path="/add" component={DisplayForm} />
+        <Route path="/game" component={DisplayGame} />
+        <Route path="/dictionary" component={DisplayDictionary} />
+      </Route>
     </DictionaryProvider>
   );
 };
